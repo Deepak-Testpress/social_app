2a333c4dd2b4a90d5d1ad6f5b7bb76b18a2bf27d
Deepak-Testpress <deepak@testpress.in>
2022-12-01 11:36:01 +0530

    Format all existing files by using black

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s(lm1rm$jjxuypr3dozvgcw#j^vm&(+p3h9h*v(otzgepp8((-"
+SECRET_KEY = (
+    "django-insecure-s(lm1rm$jjxuypr3dozvgcw#j^vm&(+p3h9h*v(otzgepp8((-"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
