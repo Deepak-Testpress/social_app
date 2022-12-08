f5a1e1ab606693b56e8a571847623a3486420bf1
Deepak-Testpress <deepak@testpress.in>
2022-12-08 15:49:06 +0530

    Add and register profile model to account app

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -141,3 +141,6 @@ LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
