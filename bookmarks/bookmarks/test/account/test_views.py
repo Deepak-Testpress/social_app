686b30b16fc84e357258eedd0336e5f5966a8cd7
Deepak-Testpress <deepak@testpress.in>
2022-12-08 15:36:01 +0530

    Support user registeration

--- bookmarks/bookmarks/test/account/test_views.py
+++ bookmarks/bookmarks/test/account/test_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase, Client
 from .test_model_mixin import ModelMixinTestCase
 from django.urls import reverse
+from account.forms import UserRegistrationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 class UserLogin(ModelMixinTestCase, TestCase):
@@ -11,3 +14,32 @@ class UserLogin(ModelMixinTestCase, TestCase):
         response = self.client.get(reverse("dashboard"))
 
         self.assertTemplateUsed(response, "account/dashboard.html")
+
+
+class UserRegister(ModelMixinTestCase, TestCase):
+    def test_register_returns_user_registration_form(self):
+        response = self.client.get(reverse("register"))
+
+        self.assertEqual(
+            type(response.context.get("form")), UserRegistrationForm
+        )
+
+    def test_registeration_templates_used(self):
+        response = self.client.get(reverse("register"))
+
+        self.assertTemplateUsed(
+            response, "account/register.html", "account/register_done.html"
+        )
+
+    def test_registeration_succeeds_with_valid_user_credentials(self):
+        self.client.post(
+            reverse("register"),
+            data={
+                "username": "johndoe",
+                "first_name": "John",
+                "email": "johndoe8@example.com",
+                "password": "johnpassword",
+                "password2": "johnpassword",
+            },
+        )
+        self.assertTrue(User.objects.filter(username="john").exists())
