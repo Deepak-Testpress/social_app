5cfbeea4ed6631160d35afce60bd9fe4a50a8bf9
Deepak-Testpress <deepak@testpress.in>
2022-12-08 14:30:35 +0530

    Add password changing support to the account

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
